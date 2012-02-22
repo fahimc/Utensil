9fcfb199374a84e87b1b73c4ba56a5d223ee5b47
fahimc <fahim@gmx.com>
2012-02-22 09:34:15 -0800

    added execcommand demo

--- src/CommandMain.js
+++ src/CommandMain.js
@@ -9,56 +9,81 @@
 	}
 
 	function loadComplete() {
-		document.getElementById('boldButton').addEventListener("click",SetToBold);
+		document.getElementById('boldButton').addEventListener("click", SetToBold);
+		document.getElementById('imageButton').addEventListener("click", addImage);
 	}
 
 	function SetToBold() {
-		execCommandOnElement(document.getElementById('copy'),"Bold");
+		execCommandOnElement(document.getElementById('copy'), "Bold");
 	}
+	function addImage()
+	{
+		execCommandOnElement(document.getElementById('copy'), "insertImage","http://levelselect.co.uk/wp-content/uploads/2008/03/nokonoko02.jpg");
+		var imgs =document.getElementById('copy').getElementsByTagName("img");
+		startDrag(imgs[imgs.length-1],0,0,200,200);
+		
+	}
+
 	function execCommandOnElement(el, commandName, value) {
-    if (typeof value == "undefined") {
-        value = null;
-    }
+		if( typeof value == "undefined") {
+			value = null;
+		}
 
-    if (typeof window.getSelection != "undefined") {
-        // Non-IE case
-        var sel = window.getSelection();
+		if( typeof window.getSelection != "undefined") {
+			// Non-IE case
+			var sel = window.getSelection();
+			console.log("here",sel);
 
-        // Save the current selection
-        var savedRanges = [];
-        for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-            savedRanges[i] = sel.getRangeAt(i).cloneRange();
-        }
+			// Save the current selection
+			var savedRanges = [];
+			for(var i = 0, len = sel.rangeCount; i < len; ++i) {
+				savedRanges[i] = sel.getRangeAt(i).cloneRange();
+			}
 
-        // Temporarily enable designMode so that
-        // document.execCommand() will work
-        document.designMode = "on";
+			// Temporarily enable designMode so that
+			// document.execCommand() will work
+			document.designMode = "on";
 
-        // Select the element's content
-        sel = window.getSelection();
-        var range = document.createRange();
-        range.selectNodeContents(el);
-        sel.removeAllRanges();
-        sel.addRange(range);
+			// Select the element's content
+			sel = window.getSelection();
+			var range = document.createRange();
+			range.selectNodeContents(el);
+			//sel.removeAllRanges();
+			//sel.addRange(range);
 
-        // Execute the command
-        document.execCommand(commandName, false, value);
+			// Execute the command
+			document.execCommand(commandName, false, value);
 
-        // Disable designMode
-        document.designMode = "off";
+			// Disable designMode
+			document.designMode = "off";
 
-        // Restore the previous selection
-        sel = window.getSelection();
-        sel.removeAllRanges();
-        for (var i = 0, len = savedRanges.length; i < len; ++i) {
-            sel.addRange(savedRanges[i]);
-        }
-    } else if (typeof document.body.createTextRange != "undefined") {
-        // IE case
-        var textRange = document.body.createTextRange();
-        textRange.moveToElementText(el);
-        textRange.execCommand(commandName, false, value);
-    }
-}
+			// Restore the previous selection
+			sel = window.getSelection();
+			sel.removeAllRanges();
+			for(var i = 0, len = savedRanges.length; i < len; ++i) {
+				sel.addRange(savedRanges[i]);
+			}
+		} else if( typeof document.body.createTextRange != "undefined") {
+			// IE case
+			var textRange = document.body.createTextRange();
+			textRange.moveToElementText(el);
+			textRange.execCommand(commandName, false, value);
+		}
+
+	}
+
+	function getSelectedText() {
+		var txt = '';
+		if(window.getSelection) {
+			txt = window.getSelection();
+		} else if(document.getSelection) {
+			txt = document.getSelection();
+		} else if(document.selection) {
+			txt = document.selection.createRange().text;
+		} else
+			return;
+		return txt;
+	}
+
 	Main()
 }(window));
