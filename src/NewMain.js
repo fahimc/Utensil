4293b329ad931ac1a78c33e79b9a778c69a9f134
fahimc <fahim@gmx.com>
2012-03-05 14:51:42 -0800

    cleaned up new demo

--- src/NewMain.js
+++ src/NewMain.js
@@ -1,12 +1,5 @@
 ( function(window) {
-	var obj = {
-		att : "hello",
-		get : function() {
-			return this.att;
-		}
-	}
-	var new_obj = {
-	};
+	
 	function Main() {
 		if(window.addEventListener) {
 			window.addEventListener("load", onLoad);
@@ -16,13 +9,15 @@
 	}
 
 	function onLoad(event) {
-		Utensil.tween(document.getElementById('box'),1,{left:"200px",top:"200px",opacity:0.5,scale:0.5},"ease-out");
-		Utensil.ImageLoader("http://2.bp.blogspot.com/-vkGvilF2Va4/ThIin4ayhSI/AAAAAAAAGjw/SrAAwq-LjnY/s1600/M11%2BOS%2BSct.jpg",onComplete);
+		var box = document.getElementById('box');
+		var _ = Utensil;
+		_.tween(box,1,{left:"200px",top:"200px",opacity:0.5,scale:0.5},"ease-out",onComplete);
+		
 	}
 	
-	function onComplete(t,x)
+	function onComplete()
 	{
-		Utensil.log("done");
+		_.log("done");
 	}
 	Main();
 }(window));
